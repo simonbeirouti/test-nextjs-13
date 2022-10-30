7632e99f13db67361433b3d96d09cd6791d0efec
simonbeirouti <hello@simonbeirouti.com>
2022-10-30 23:38:23 +1100

    use reset button to error boundaries

--- app/posts/[postId]/error.jsx
+++ app/posts/[postId]/error.jsx
@@ -1,11 +1,21 @@
 "use client";
-
+import { useEffect } from "react";
 import Hero from "../../../components/Hero";
 
-export default function IssueErrorPage({ error }) {
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
   return (
     <div className="flex justify-center items-center">
       <Hero intro="Error" text={error.message} />
+      <button
+        className=" text-black text-center block px-3 py-2 text-base font-medium bg-gray-100 border rounded-xl hover:bg-gray-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+        onClick={() => reset()}
+      >
+        Reset error
+      </button>
     </div>
   );
 }
