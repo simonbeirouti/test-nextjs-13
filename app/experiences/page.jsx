2d77331a7f521952eb1e473b992c8d4232444470
simonbeirouti <hello@simonbeirouti.com>
2022-10-31 19:08:48 +1100

    create proper experience page

--- app/experiences/page.jsx
+++ app/experiences/page.jsx
@@ -1,14 +1,23 @@
-import Hero from "../../components/Hero";
-import ExperienceList from "../../components/ExperienceList";
+import { PrismaClient } from "@prisma/client";
+import Hero from "../../components/utilities/Hero";
+import ExperienceCard from "../../components/experiences/ExperienceCard";
 
-export default function page() {
+export default async function page() {
+  const prisma = new PrismaClient();
+  const experiences = await prisma.experience.findMany();
   return (
     <>
       <Hero
         intro="Experiences"
         text="Truthful ratings on different things that I experience in life."
       />
-      <ExperienceList />
+      <section className="py-6 sm:py-8 lg:py-10">
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="grid grid-cols-1 gap-4 sm:gap-6 lg:grid-cols-4">
+            <ExperienceCard experiences={experiences} />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
