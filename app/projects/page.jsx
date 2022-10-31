181756df286ca43d60eb060da0c81adc22279bd3
simonbeirouti <hello@simonbeirouti.com>
2022-10-31 19:09:21 +1100

    create project page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,14 +1,23 @@
-import Hero from "../../components/Hero";
-import ProjectList from "../../components/ProjectList";
+import Hero from "../../components/utilities/Hero";
+import { PrismaClient } from "@prisma/client";
+import ProjectCard from "../../components/projects/ProjectCard";
 
-export default function page() {
+export default async function page() {
+  const prisma = new PrismaClient();
+  const projects = await prisma.project.findMany();
   return (
     <>
       <Hero
         intro="Projects"
         text="The various projects I'm creating a link directly to them"
       />
-      <ProjectList />
+      <section className="py-6 sm:py-8 lg:py-10">
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="grid grid-cols-1 gap-6 mt-12 sm:grid-cols-2 lg:grid-cols-4">
+            <ProjectCard projects={projects} />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
