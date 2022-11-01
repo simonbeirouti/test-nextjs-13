384bea2b9b69606f39c79e50f4b71c41e279984e
simonbeirouti <hello@simonbeirouti.com>
2022-11-01 19:34:19 +1100

    update prisma to postgresql

--- prisma/migrations/20221101083230_local_to_live/migration.sql
+++ prisma/migrations/20221101083230_local_to_live/migration.sql
@@ -1,37 +1,45 @@
 -- CreateTable
 CREATE TABLE "Post" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "date" TEXT NOT NULL,
     "image" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "category" TEXT NOT NULL
+    "category" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Project" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "date" TEXT NOT NULL,
     "snippet" TEXT NOT NULL,
     "image" TEXT NOT NULL,
-    "url" TEXT NOT NULL
+    "url" TEXT NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Experience" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "date" TEXT NOT NULL,
     "summary" TEXT NOT NULL,
     "image" TEXT NOT NULL,
     "rating" TEXT NOT NULL,
-    "url" TEXT NOT NULL
+    "url" TEXT NOT NULL,
+
+    CONSTRAINT "Experience_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Stat" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
-    "value" TEXT NOT NULL
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "Stat_pkey" PRIMARY KEY ("id")
 );
