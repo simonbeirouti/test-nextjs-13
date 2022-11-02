7ae1f6a018c8714d84551725c80c9a4d60a0a9f7
simonbeirouti <hello@simonbeirouti.com>
2022-11-02 14:05:51 +1100

    add equals to post to find single post

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-
 import PostDetails from "../../../components/posts/PostDetails";
 
 export default async function IssueDetailsPage({ params }) {
   const prisma = new PrismaClient();
 
   const post = await prisma.post.findFirst({
-    where: { id: +params.postId },
+    where: { id: { equals: +params.postId } },
   });
 
   if (!post) {
