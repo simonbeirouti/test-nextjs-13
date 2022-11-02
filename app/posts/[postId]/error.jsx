cecc63f6b1f41db05a8db160b5d2e07a4903a57c
simonbeirouti <hello@simonbeirouti.com>
2022-11-02 14:06:09 +1100

    rmv error button

--- app/posts/[postId]/error.jsx
+++ app/posts/[postId]/error.jsx
@@ -12,14 +12,6 @@ export default function Error({ error, reset }) {
       <div className="flex justify-center items-center">
         <Hero intro="Error" text={error.message} />
       </div>
-      <div>
-        <button
-          className=" text-black text-center block px-3 py-2 text-base font-medium bg-gray-100 border rounded-xl hover:bg-gray-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-          onClick={() => reset()}
-        >
-          Reset error
-        </button>
-      </div>
     </>
   );
 }
