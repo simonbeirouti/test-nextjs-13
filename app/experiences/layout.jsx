54f14c97ce75cf692ae10e34afb5ca019f3d58d4
simonbeirouti <hello@simonbeirouti.com>
2022-11-06 12:45:00 +1100

    add experience head file and add it to layout

--- app/experiences/layout.jsx
+++ app/experiences/layout.jsx
@@ -3,6 +3,7 @@ import Hero from "../../components/utilities/Hero";
 export default async function ProjectLayout({ children }) {
   return (
     <>
+      <head />
       <Hero
         intro="Experiences"
         text="Truthful ratings on different things that I experience in life."
