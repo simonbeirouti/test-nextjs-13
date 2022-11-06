01ef3ce5c1e74d6eff02c7cb29c18370441c2231
simonbeirouti <hello@simonbeirouti.com>
2022-11-06 12:45:15 +1100

    add projects head file and add it to layout

--- app/projects/layout.jsx
+++ app/projects/layout.jsx
@@ -3,6 +3,7 @@ import Hero from "../../components/utilities/Hero";
 export default async function ProjectLayout({ children }) {
   return (
     <>
+      <head />
       <Hero
         intro="Projects"
         text="The various projects I'm creating a link directly to them"
